6997246ae3df70354135cec594f025d01e44a4e7
Trung <trungnguyen.02082000@gmail.com>
2023-12-27 20:29:03 +0700

    hoàn thành

---
+++ index.js
@@ -0,0 +1,15 @@
+// $(document).ready(function () {
+//     const header = document.querySelector("header");
+//     const dropdown = document.querySelector(".dropdown");
+
+//     // Lấy chiều rộng của dropdown
+//     const dropdownWidth = dropdown.offsetWidth;
+
+//     // Mở rộng header cho bằng chiều rộng dropdown
+//     header.style.width = dropdownWidth;
+
+//     // Khi dropdown được ẩn, thu gọn header về chiều rộng ban đầu
+//     dropdown.addEventListener("hidden.bs.dropdown", function () {
+//         header.style.width = "auto";
+//     });
+// });
